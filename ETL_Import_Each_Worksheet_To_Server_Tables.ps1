f1fad80adce9d25f71569c9eadc2548248691189
Brennan_Webb <brennan.webb@selectquote.com>
2022-06-21 14:07:48 -0400

    added drop table function

--- ETL_Import_Each_Worksheet_To_Server_Tables.ps1
+++ ETL_Import_Each_Worksheet_To_Server_Tables.ps1
@@ -3,22 +3,29 @@
 The following code will take in the path of an excel workbook and loop through each worksheet.
 Each worksheet will be written to table under the designated server.database.schema.worksheet name.
 The -force tag will overwrite any existing tables.
+The $drop variable will drop the table before load.
 Powershell 5.0, 7.1.4 tested.
 Required Modules: Install-Module -Name ImportExcel, SqlServer;
 #>
 
-[string] $IntakeFilePath ="{File Path}";
-$Worksheets=Get-ExcelSheetInfo -Path $IntakeFilePath #generates a table for
+[string] $IntakeFilePath ="{File Path}"; #Path must be valid.
+$Worksheets=Get-ExcelSheetInfo -Path $IntakeFilePath; #generates a table for each worksheet in excel file.
 $Server ="{Server}";
 $Db="{Database}";
-$Schema ="{Schema}"
+$Schema ="{Schema}";
 $IsFile = (Get-Item $IntakeFilePath) -is [System.IO.FileInfo];
+$Drop = 0;
 
 #Setup a function which will loop and return what the affected record count.
 function ETL_to_Server {
     foreach ($Worksheet in $Worksheets) {
-        $TableName = $Worksheet.Name.Replace(".","_");
-        $Worksheet = $Worksheet.Name
+        $TableName = $Worksheet.Name.Replace(".","_").replace(" ","_");
+        $Worksheet = $Worksheet.Name; Write-Host "Worksheet Name: "$Worksheet;
+        If ($Drop=1)
+        {
+            $Query = "Drop Table If Exists "+$Schema+"."+$TableName
+            Invoke-Sqlcmd -query $Query -ServerInstance $Server -database $Db;
+        }
         ,(Import-Excel -Path $IntakeFilePath -WorksheetName $Worksheet -AsText *) |
         Write-SqlTableData -ServerInstance $Server -Database $Db -SchemaName $Schema -TableName $TableName -Force;
     }   
