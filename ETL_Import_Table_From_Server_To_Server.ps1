052cac6043f7c7e2035236e0c4acd3b926f0061c
Brennan_Webb <brennan.webb@selectquote.com>
2022-08-18 10:53:28 -0400

    minor changes

--- ETL_Import_Table_From_Server_To_Server.ps1
+++ ETL_Import_Table_From_Server_To_Server.ps1
@@ -1,11 +1,11 @@
 
 <#Overview:
 The following code will copy results from server table to another server table.
-The -force tag will overwrite any existing tables.
 The $drop variable will drop the table before load.
 Powershell 5.0, 7.1.4, 7.2.5 tested.
-Required Modules: Install-Module -Name ImportExcel, SqlServer;
+Required Modules: Install-Module -Name SqlServer;
 #>
+
 
 #Source Vars
 $SourceServer ="warehouse.selectquote.com";
@@ -20,20 +20,26 @@ $TargetSchema ="dbo";
 $TargetTable ="COMP_CoreCompReportingMonths"
 $TargetAction = "Trunc" #Drop, Trunc
 
+function ReadWrite {
+    ,(Read-SqlTableData -ServerInstance $SourceServer -Database $SourceDb -SchemaName $SourceSchema -TableName $SourceTable) |
+    Write-SqlTableData -ServerInstance $TargetServer -Database $TargetDb -SchemaName $TargetSchema -TableName $TargetTable;  
+}
 
 Switch($TargetAction)
 {
     Drop {
         $Query = 'Drop Table If Exists '+$TargetSchema+'.'+$TargetTable
         Invoke-Sqlcmd -query $Query -ServerInstance $TargetServer -database $TargetDb;
+        ReadWrite;
     }
 
     Trunc{
         $Query = 'Truncate Table '+$TargetSchema+'.'+$TargetTable
         Invoke-Sqlcmd -query $Query -ServerInstance $TargetServer -database $TargetDb;
+        ReadWrite;
     }
     
 };
-,(Read-SqlTableData -ServerInstance $SourceServer -Database $SourceDb -SchemaName $SourceSchema -TableName $SourceTable) |
-Write-SqlTableData -ServerInstance $TargetServer -Database $TargetDb -SchemaName $TargetSchema -TableName $TargetTable;
+
+
 
