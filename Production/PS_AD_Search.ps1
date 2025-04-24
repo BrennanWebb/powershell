e25d66fcb8a43952ed4d15f9a3907a46752347d3
Brennan_Webb <brennan.webb@selectquote.com>
2025-04-24 13:40:12 -0400

    fixed welcome header

--- Production/PS_AD_Search.ps1
+++ Production/PS_AD_Search.ps1
@@ -267,7 +267,7 @@ if ($UserName) {
 
 # Interactive main loop for user choice
 while ($true) {
-    Write-ArtsyHeader -title "Welcome     " -foregroundcolor Green
+    Write-ArtsyHeader -title "Welcome   " -foregroundcolor Green
     Write-Host "Choose Search Type" -ForegroundColor Cyan
     Write-Host "[1] User Search"
     Write-Host "[2] Group Search"
